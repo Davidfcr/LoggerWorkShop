616c2d277e1d079c8b98008776df8f26881d665d
David Cardenas <cardenasd@surgent.com>
2018-12-12 11:11:31 -0500

    Update to the guide and the LoggerSQL project

--- LoggerSQL/LoggerSQL/Program.cs
+++ LoggerSQL/LoggerSQL/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
-namespace LoggerSQL
+﻿namespace LoggerSQL
 {
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using System;
+    using System.IO;
+
     public class Program
     {
         public static void Main(string[] args)
